910f9bff1057f5c174386dacd96d9ecd4208c387
mathieu.boldo <mathieu.boldo@entrili.com>
2024-02-26 10:15:42 +0100

    Implement php-xapi/lrs-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class            => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class             => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    XApi\LrsBundle\XApiLrsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
---
+++ src/EventSubscriber/ResponseHeadersSubscriber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ResponseHeadersSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [KernelEvents::RESPONSE => 'onKernelResponse'];
+    }
+
+    /**
+     * @param ResponseEvent $event
+     */
+    public function onKernelResponse(ResponseEvent $event): void
+    {
+        $headers = $event->getResponse()->headers;
+
+        $headers->add([
+            'Access-Control-Allow-Origin'  => '*',
+            'Access-Control-Allow-Methods' => 'POST, GET, PUT, OPTIONS',
+            'Access-Control-Allow-Headers' => 'Accept, Authorization, Content-Length, Content-Type, ETag, Last-Modified, Status, X-Experience-API-Version, X-Experience-API-Consistent-Through',
+        ]);
+    }
+}
